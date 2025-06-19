c64608cb6ab3fa37c44acef001f5fb772777b816
ravinder <ravibeniwal931@gmail.com>
2025-06-19 20:14:01 +0530

    some new text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,11 @@
 'use client';
 import React from "react";
-import { motion } from "motion/react";
+import { easeOut, motion } from "motion/react";
 import { LampContainer } from '@/components/ui/lamp'
 
 const Hero = () => {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen bg-black text-white text-center px-4 py-2 bg-gradient-to-bl from-[#0f1a2f] to-transparent ">
+    <section className="flex flex-col items-center justify-center min-h-screen bg-black text-white text-center  bg-gradient-to-bl from-[#0f1a2f] to-transparent ">
         <LampContainer>
       <motion.h1
         initial={{ opacity: 0.5, y: 100 }}
@@ -17,19 +17,20 @@ const Hero = () => {
         }}
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
-        RAVI <br /> PORTFOLIO
+        Hey! I am
+  
+      </motion.h1>
+      <br />
+      
+      <motion.h1
+      initial={{ opacity: 0, y: 30 }}
+      animate ={{opacity : 1 , y : 0}}
+      transition={{duration : 1 , ease : 'easeOut'}}
+      className="text-5xl md:text-8xl font-extrabold text-center bg-gradient-to-r from-cyan-500 via-slate-300 to-cyan-500 text-transparent bg-clip-text"
+      >
+        Ravi Benwial
       </motion.h1>
     </LampContainer>
-      <p className="text-blue-400 font-medium mb-2">Design in <span className="underline underline-offset-4">Details</span></p>
-      
-      <h1 className="text-6xl sm:text-8xl font-extrabold tracking-tight relative">
-        PORTFOLIO
-        <span className="absolute left-1/2 transform -translate-x-1/2 text-[10rem] text-transparent bg-clip-text bg-gradient-to-r from-gray-400 to-white">
-          {/* You can insert a 3D logo overlay here or use a component */}
-        </span>
-      </h1>
-
-      <p className="text-yellow-400 mt-4 text-xl font-signature">Ravi</p>
     </section>
   );
 };
