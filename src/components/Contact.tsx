1e2a61159b8cdd1502ec1348aaf0bdc3c8550f7c
ravinder <ravibeniwal931@gmail.com>
2025-06-21 19:08:33 +0530

    connect section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='bg-white w-full h-screen flex items-center justify-center'>
+      <div className='bg-black w-11/12 max-w-4xl min-h-[500px] flex flex-row items-center justify-center gap-3 mx-4 sm:mx-8 md:mx-auto p-4 rounded-lg '>
+        <div className='w-160 h-120 bg-neutral-800 rounded-lg border-white border'>
+          <h1 className='text-white text-2xl font-extrabold mt-5 mx-4 '>Connect With Me</h1>
+          <div className='flex flex-col'>
+            <div className='mt-5 mx-4 flex flex-row gap-3'>
+            <label htmlFor="">First Name</label>
+             <input type="text" placeholder='First Name' />
+             
+
+          </div>
+          </div>
+          
+        </div>
+        <div className='w-120 h-120 bg-neutral-800 rounded-lg border-white border'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
