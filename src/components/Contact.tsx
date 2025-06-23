f6db27f6ead678430144b340cd8cb7fa8b4874e5
ravinder <ravibeniwal931@gmail.com>
2025-06-23 18:21:58 +0530

    footer changes

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,25 +1,68 @@
+'use client'
 import React from 'react'
+import { motion } from "motion/react";
 
 const Contact = () => {
   return (
     <div className='bg-white w-full h-screen flex items-center justify-center'>
-      <div className='bg-black w-11/12 max-w-4xl min-h-[500px] flex flex-row items-center justify-center gap-3 mx-4 sm:mx-8 md:mx-auto p-4 rounded-lg '>
+      <motion.div
+      initial = {{
+        opacity : 0,
+        y : 100 ,
+      }}
+      whileInView={{
+        opacity : 1 ,
+        y : 0,
+      }}
+      transition={{
+        duration : 0.9,
+        delay : 0.3,
+      }}
+       className='bg-black w-11/12 max-w-4xl min-h-[500px] flex flex-row items-center justify-center gap-3 mx-4 sm:mx-8 md:mx-auto p-4 rounded-lg '>
+      {/* Form Section  */}
         <div className='w-160 h-120 bg-neutral-800 rounded-lg border-white border'>
           <h1 className='text-white text-2xl font-extrabold mt-5 mx-4 '>Connect With Me</h1>
-          <div className='flex flex-col'>
-            <div className='mt-5 mx-4 flex flex-row gap-3'>
-            <label htmlFor="">First Name</label>
-             <input type="text" placeholder='First Name' />
-             
-
-          </div>
-          </div>
           
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-col-2 max-xl:grid-cols-2 gap-3'>
+              <div className='mt-3 mx-4 flex flex-col gap-2'>
+                  <label htmlFor="" className='text-white text-md font-bold'>First Name</label>
+                  <input className='bg-neutral-400 text-white rounded-sm ' type="text" placeholder='First Name' />
+               </div>
+               <div className='mt-3 mx-4 flex flex-col gap-2  '>
+                  <label htmlFor="" className='text-md font-bold text-white'>Last Name</label>
+                  <input type="text" className='bg-neutral-400 text-white rounded-sm ' placeholder='Last Name' />
+               </div>
+               <div className='mt-3 mx-4 flex flex-col gap-2  '>
+                  <label htmlFor="" className='text-md font-bold text-white'>Email</label>
+                  <input type="text" className='bg-neutral-400 text-white rounded-sm ' placeholder='Email' />
+               </div>
+               <div className='mt-3 mx-4 flex flex-col gap-2  '>
+                  <label htmlFor="" className='text-md font-bold text-white'>Ph: number</label>
+                  <input type="text" className='bg-neutral-400 text-white rounded-sm' placeholder='Ph: number' />
+               </div>
+            </div>
+           <div className='mt-5 mx-4 flex flex-col gap-2  '>
+                  <label htmlFor="" className='text-md font-bold text-white'>Your requirment</label>
+                  <div className='grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-3 '>
+                    <button className='w-auto h-auto  bg-neutral-500 text-white rounded-sm py-1 border'>Frontend Devlopment</button>
+                   <button className='w-auto h-auto  bg-neutral-500 text-white rounded-sm py-1 border border-white'>Backend Devlopment</button>
+                    <button className='w-auto h-auto  bg-neutral-500 text-white rounded-sm py-1'>Web3 Devlopment</button>
+                    <button className='w-auto h-auto  bg-neutral-500 text-white rounded-sm py-1'>AI Research </button>
+                  </div> 
+               </div>
+
+               <div className=' mx-4 mt-4 flex flex-col'>
+                <label htmlFor="" className=' text-md font-bold text-white'>How can i help you ?</label>
+                <textarea name="message" id="message" rows={4}
+                className="overflow-hidden resize-none mt-2 text-md text-white font-bold h-15 w-auto rounded-sm bg-neutral-400 px-3 py-2" 
+                 placeholder='Tell me about your project, goals, and how I can assist you...'
+                 required ></textarea>
+               </div>
         </div>
         <div className='w-120 h-120 bg-neutral-800 rounded-lg border-white border'>
 
         </div>
-      </div>
+      </motion.div>
     </div>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,102 @@
-import React from 'react'
+"use client"
+import React, { useRef } from 'react'
+import { IconHome, IconBriefcase , IconBrandGithub, IconBrandLinkedin, IconBrandX, IconMail } from '@tabler/icons-react';
+import { motion, useMotionValue, useTransform } from 'motion/react';
+import { MotionValue } from 'motion/react';
+import Link from 'next/link';
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-white h-screen w-full relative'>
+      <FloatingFooter/>
+    </div>
   )
 }
 
-export default Footer
+export default Footer;
+
+type Link = {
+  title : string,
+  icon : React.ReactNode | any,
+  href : string
+}
+
+const FloatingFooter = () => {
+  
+  const links : Link[] = [
+    {
+    title : "Home",
+    icon : <IconHome className='h-full w-full text-neutral-300'/>, 
+    href : '/'
+     },
+     {
+      title : "Projects",
+      icon : <IconBriefcase className='h-full w-full text-neutral-300'/>,
+      href : '/'
+     },
+     {
+      title : "Github",
+      icon : <IconBrandGithub className='h-full w-full text-neutral-300'/>,
+      href : '/'
+     },
+     {
+      title : "Linkdin",
+      icon : <IconBrandLinkedin className='h-full w-full text-neutral-300'/>,
+      href :'/'
+     },
+     {
+      title : "X",
+      icon :  <IconBrandX className='h-full w-full text-neutral-300'/>,
+      href : '/'
+     },
+     {
+      title : "Email",
+      icon : <IconMail className='h-full w-full text-neutral-300'/>,
+      href : '/'
+     }
+   ]
+
+  const mouseX = useMotionValue(Infinity);
+ 
+  return (
+    <motion.div
+      onMouseMove={(e) => mouseX.set(e.clientX)}
+      onMouseLeave={(e) => mouseX.set(Infinity)}
+      className='absolute flex justify-center items-center gap-3 bottom-10 inset-x-0 mx-auto h-16 w-fit bg-neutral-500 rounded-2xl px-4'>
+      {links.map((el,idx) => (
+        <IconContainer mouseX={mouseX} key={el.title} el={el}/>
+      ))}
+    </motion.div>
+  )
+}
+
+export const IconContainer = ({el,mouseX} : {el : Link;mouseX : MotionValue<number>}) =>{
+
+  const ref = useRef<HTMLDivElement>(null);
+  const distance = useTransform(
+    mouseX,(val)=>{
+      let bound = ref?.current?.getBoundingClientRect() ?? {x:0,width :0};
+      return val-bound.x-bound.width/2;
+    }
+  );
+
+  let width = useTransform(distance,[-100,0,100],[40,80,40]);
+  let height = useTransform(distance,[-100,0,100],[40,80,40]);
+  let rotate = useTransform(distance,[-100,0,100],[-20,0,20]);
+
+  return (
+    <Link href={el.href}>
+     <motion.div
+     ref = {ref}
+     style ={{
+      width : width,
+      height : height,
+      
+     }}
+      className='bg-neutral-700 h-12 w-12 flex items-center justify-center rounded-md'>
+      {el.icon}
+      </motion.div>
+     </Link>
+  )
+}
